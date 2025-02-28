4ed93a4e20ed931011e7dd4868d465be8478a311
Thibault Desmoulins <thibault.desmoulins@gmail.com>
2025-02-28 09:48:30 +0100

    Task 5: Update API_PATHS in order to add import lambda URL

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://1tmvuptf84.execute-api.eu-west-3.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-3.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-3.amazonaws.com/dev",
+  import: "https://sw0zfeofcb.execute-api.eu-west-3.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-3.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-3.amazonaws.com/dev",
 };
