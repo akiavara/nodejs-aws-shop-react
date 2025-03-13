90f1eb185196585b4ebd434c3fc30c9820b90e38
Thibault Desmoulins <thibault.desmoulins@gmail.com>
2025-03-13 17:35:17 +0100

    Task 7: Add authorization-service module

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import axios from "axios";
+import { useAlert } from "~/components/Alert/AlertContext";
 
 type CSVFileImportProps = {
   url: string;
@@ -10,6 +11,7 @@ type CSVFileImportProps = {
 
 export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const [file, setFile] = React.useState<File>();
+  const { showAlert } = useAlert();
 
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
@@ -30,14 +32,63 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
       return;
     }
 
-    // Get the presigned URL
-    const response = await axios({
-      method: "GET",
-      url,
-      params: {
-        name: encodeURIComponent(file.name),
-      },
-    });
+    // Get the authorization token from localStorage
+    const authorization_token = localStorage.getItem("authorization_token");
+    if (!authorization_token) {
+      showAlert("Authorization token not found. Please log in again.", "error");
+      return;
+    }
+
+    console.log(`Authorization = Basic ${authorization_token}`);
+
+    let response;
+
+    try {
+      // Get the presigned URL
+      response = await axios({
+        method: "GET",
+        headers: { Authorization: `Basic ${authorization_token}` },
+        url,
+        params: {
+          name: encodeURIComponent(file.name),
+        },
+      });
+
+      if (response.status === 401) {
+        showAlert("Unauthorized: Please log in again", "error");
+        return;
+      }
+
+      if (response.status === 403) {
+        showAlert("Forbidden: Invalid credentials", "error");
+        return;
+      }
+    } catch (error: any) {
+      //showAlert("Unauthorized: Please log in again", "error");
+      if (axios.isAxiosError(error)) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          if (error.response.status === 401) {
+            showAlert("Unauthorized: Please log in again", "error");
+          } else if (error.response.status === 403) {
+            showAlert("Forbidden: Invalid credentials", "error");
+          } else {
+            showAlert("Error: Unknown error", "error");
+          }
+        } else if (error.request) {
+          // The request was made but no response was received
+          showAlert("No response received from server", "error");
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          showAlert("Error setting up request: " + error.message, "error");
+        }
+      } else {
+        // Something else happened
+        showAlert("An unexpected error occurred", "error");
+      }
+      return;
+    }
 
     console.log("File to upload: ", file.name);
     console.log("Uploading to: ", response.data);
@@ -45,12 +96,18 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
     const result = await fetch(response.data, {
       method: "PUT",
       headers: {
-        'Content-Type': 'text/csv'
+        "Content-Type": "text/csv",
       },
       body: file,
     });
 
     console.log("Result: ", result);
+
+    if (result.ok) {
+      showAlert("File uploaded successfully", "success");
+    } else {
+      showAlert("Error uploading file", "error");
+    }
 
     setFile(undefined);
   };
